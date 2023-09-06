0038e6d4cb779ad5f95795b4de5b15cbd609ab8d
leandert <leandert@stud.ntnu.no>
2023-09-06 23:09:10 +0200

    ✨ Created function to get api query for point
    Created a function that returns a MET locationforecast 2.0 query for a
    specific point. Also created some default destinations.

---
+++ src/configReader.tsx
@@ -0,0 +1,13 @@
+interface Point {
+  name: string;
+  lat: number;
+  lon: number;
+  alt: number;
+}
+
+//Parses a Point object into an API query
+function getPointQuery(point: Point): string {
+  const { lat, lon, alt } = point;
+  return 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=' +
+      lat + '&lon=' + lon + '&altitude=' + alt;
+}
