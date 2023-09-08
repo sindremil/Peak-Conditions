7b285de187ea0c2633e03aacaa566e7c8346cc97
sindreeh <sindreeh@stud.ntnu.no>
2023-09-08 15:30:44 +0200

    feat: :sparkles: Added location image and icon to destination card
    Added location image and icon to destination card, as well as styling.

--- src/component/DestinationCardComponent.tsx
+++ src/component/DestinationCardComponent.tsx
@@ -1,8 +1,15 @@
+import zermatt from "./../assets/zermatt.jpg"
+import cloudy from "./../assets/cloudy.svg"
+
 export default function DestinationCard() {
   return (
     <div className="destinationCard">
-      <div>Zermatt</div>
-      <div>0°  3m/s</div>
+      <img className="destinationCardImg" src={zermatt}/>
+      <div className="destinationCardInfo">
+        <div>Zermatt</div>
+        <div>0°  3m/s</div>
+      </div>
+      <img className="weatherIcon" src={cloudy}/>
     </div>
   )
 }
