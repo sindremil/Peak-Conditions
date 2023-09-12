f4da8d1153d1e2b4f3c6bd1430275627c9ed0036
sindreeh <sindreeh@stud.ntnu.no>
2023-09-12 13:45:34 +0200

    feat: :sparkles: Added the favorite button to destination card
    Added the favorite button, and functionality to toggle.

--- src/component/DestinationCardComponent.tsx
+++ src/component/DestinationCardComponent.tsx
@@ -1,8 +1,23 @@
-import zermatt from "./../assets/zermatt.jpg"
-import partlycloudy from "./../assets/partlycloudy.png"
-import partlycloudyalt from "./../assets/partlycloudyalt.png"
+import zermatt from "./../assets/zermatt.jpg";
+import partlycloudy from "./../assets/partlycloudy.png";
+import partlycloudyalt from "./../assets/partlycloudyalt.png";
+import star1 from "./../assets/star1.png";
+import star2 from "./../assets/star2.png";
+import { useState } from "react";
 
 export default function DestinationCard() {
+
+  const [isFavorite, setIsFavorite] = useState(star1);
+
+  function handleFavorite() {
+    if (isFavorite === star1) {
+      setIsFavorite(star2);
+    }
+    else {
+      setIsFavorite(star1);
+    }
+  }
+
   return (
     <div className="destinationCard">
       <img className="destinationCardImg" src={zermatt}/>
@@ -11,6 +26,7 @@ export default function DestinationCard() {
         <div>0°  3m/s</div>
       </div>
       <img className="weatherIcon" src={partlycloudy}/>
+      <img className="favorite" onClick={handleFavorite} src={isFavorite}/>
       <div className="details">
         <hr className="destinationCardDivider"/>
         Detaljer
