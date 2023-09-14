bcd3273ec55bde2dd5f5ab097d052484712bc395
jesperrg <jesperrg@stud.ntnu.no>
2023-09-14 17:18:47 +0200

    ✨ Made App.tsx get the correct Weather data
    App.tsx now gets the wanted information from the different JSONs the API returns.
    It also displays it in a DestinationComponent.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import './App.css'
 import "./component/DestinationCardStyle.css"
 import DestinationCard from './component/DestinationCardComponent'
+import DebugComponent from './component/DebugComponent'
+import DestinationPoint from './schemas/DestinationPoint'
+import { useWeatherData } from './api/FetchWeatherData'
 
 function App() {
 
+  const destinationList = ["Åre", "Hemsedal"]
+
+  function getDestinationWeatherData(destinationName: string) {
+    const destinationPoint: DestinationPoint = {destination: destinationName, pointIndex: 0}
+    const { data, isLoading, isError } = useWeatherData(destinationPoint);
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    if (isError) {
+      return <div>Error loading weather data</div>;
+    } 
+
+    const airTemperature = data.properties.timeseries[0].data.instant.details.air_temperature;
+    const windSpeed = data.properties.timeseries[0].data.instant.details.wind_speed;
+    const symbolCode = data.properties.timeseries[0].data.next_1_hours.summary.symbol_code;
+
+    console.log(destinationPoint.destination)
+
+    return <div key={destinationPoint.destination}><DestinationCard
+    destination={destinationPoint.destination}
+    temperature={airTemperature}
+    windSpeed={windSpeed}
+    symbolCode={symbolCode}
+  /></div>
+    }
+  
+
   return (
     <div className="content">
-      <DestinationCard
-        destination="Åre"
-        temperature={3}
-        windSpeed={3}
-        symbolCode="partlycloudy"
-      />
+      {destinationList.map((destination) => (getDestinationWeatherData(destination)))}
+      <DebugComponent/>
     </div>
   )
 }
---
+++ src/component/DebugComponent.tsx
@@ -0,0 +1,33 @@
+//import { useWeatherData } from "../api/FetchWeatherData";
+
+import { useWeatherData } from "../api/FetchWeatherData";
+import DestinationPoint from "../schemas/DestinationPoint";
+
+function DebugComponent() {
+
+  const destinationPoint: DestinationPoint = {destination: "Åre", pointIndex: 0}
+  const { data, isLoading, isError } = useWeatherData(destinationPoint);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading weather data</div>;
+  } 
+
+  const airTemp = data.properties.timeseries[0].data.instant.details.air_temperature;
+  const windSpeed = data.properties.timeseries[0].data.instant.details.wind_speed;
+  const symbol = data.properties.timeseries[0].data.next_1_hours.summary.symbol_code;
+
+  return (
+    <div>
+      <p>{airTemp}</p>
+      <p>{windSpeed}</p>
+      <p>{symbol}</p>
+      <pre>{JSON.stringify(data)}</pre>
+    </div>
+  );
+}
+
+export default DebugComponent;
