a24556cc13a402e0fc2e1636c5baf045cb01ce82
jesperrg <jesperrg@stud.ntnu.no>
2023-09-20 23:53:42 +0200

    🐛Removed Devtools
    Removed Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './global.css';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
