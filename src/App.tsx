5fb41ef1ff0f6354feddcd319d5e35cd403ff0b5
leandert <leandert@stud.ntnu.no>
2023-09-28 22:04:00 +0200

    ✨Page title is now displayed

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,33 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DestinationPage from './DestinationPage';
 import Navbar from './component/NavBar';
 import getDestinationNames from './utils/getDestinationNames';
+import SetPageTitle from './utils/SetPageTitle';
 
 function App() {
   const destinationList = getDestinationNames();
   return (
     <Router basename="/project1">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        {destinationList.map((destination) => (
+    <Navbar />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <SetPageTitle title="Home" />
+            <LandingPage />
+          </>
+        }
+      />
+      {destinationList.map((destination) => (
           <Route
             key={destination}
             path={destination.toLowerCase()}
-            element={<DestinationPage destination={destination} />}
+            element={
+              <>
+                <SetPageTitle title={destination} />
+                <DestinationPage destination={destination} />
+              </>
+            }
           />
         ))}
       </Routes>
