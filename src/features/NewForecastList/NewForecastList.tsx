03e42f75ef4036c7d92d655d0cc30f9f358b3ba6
leandert <leandert@stud.ntnu.no>
2023-11-05 23:06:11 +0100

    ✨NewForecastList created with less data than old

--- src/features/NewForecastList/NewForecastList.tsx
+++ src/features/NewForecastList/NewForecastList.tsx
@@ -1,19 +1,59 @@
 import DestinationPoint from "../../interfaces/DestinationPoint";
 import { useTimeseriesData } from "../../hooks/useTimeseriesData";
+import { forecastListHours } from "../../configs/settings";
 
-export default function NewForecastList(destinationPoint: DestinationPoint) {
-
-  const { timeseriesData, isLoading, isError } = useTimeseriesData(destinationPoint, 0);
+// Entry component for each row in the table
+function ForecastListEntry({ destinationPoint, timeseriesIndex }: { destinationPoint: DestinationPoint, timeseriesIndex: number }) {
+  const { timeseriesData, isLoading, isError } = useTimeseriesData(destinationPoint, timeseriesIndex);
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <tr><td colSpan={4}>Loading...</td></tr>;
   }
   if (isError) {
-    return <p>Error fetching forecast</p>;
+    return <tr><td colSpan={4}>Error fetching forecast</td></tr>;
+  }
+
+  const time = timeseriesData?.time;
+  const weather = timeseriesData?.data.next_1_hours.summary.symbol_code;
+  const precipitation = timeseriesData?.data.next_1_hours.details.precipitation_amount;
+  const wind = timeseriesData?.data.instant.details.wind_speed;
+
+  return (
+    <tr>
+      <td>{time}</td>
+      <td>{weather}</td>
+      <td>{precipitation} mm</td>
+      <td>{wind} km/h</td>
+    </tr>
+  );
+}
+
+// Main list component to render the table
+export default function NewForecastList({ destination, pointIndex }: DestinationPoint) {
+  const rows = [];
+  for (let i = 0; i < forecastListHours; i++) {
+    rows.push(
+      <ForecastListEntry 
+        key={i}
+        destinationPoint={{ destination, pointIndex }}
+        timeseriesIndex={i}
+      />
+    );
   }
 
   return (
-    <h3>Test</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Weather</th>
+          <th>Precipitation</th>
+          <th>Wind</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
   );
-
 }
