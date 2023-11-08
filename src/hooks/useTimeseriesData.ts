4a56297ab2948bb241703bad06ff28f59b7bc33c
leandert <leandert@stud.ntnu.no>
2023-11-08 19:09:19 +0100

    ✨Formatted date in table

--- src/hooks/useTimeseriesData.ts
+++ src/hooks/useTimeseriesData.ts
@@ -1,21 +1,33 @@
-import { useEffect, useState } from 'react';
-import { useWeatherDataQuery } from './useWeatherData';
-import WeatherData from '../interfaces/WeatherData';
-import DestinationPoint from '../interfaces/DestinationPoint';
+import { useWeatherDataQuery } from "./useWeatherData";
+import DestinationPoint from "../interfaces/DestinationPoint";
 
 // Returns the forecast object at the index of a timeseries
-// Visit api docs for more info about timeseries and forecast objects 
+// Visit api docs for more info about timeseries and forecast objects
 // https://docs.api.met.no/doc/ForecastJSON.html
-export const useTimeseriesData = (destinationPoint: DestinationPoint, index: number) => {
-  const { data, isLoading, isError } = useWeatherDataQuery(destinationPoint);
-  const [timeseriesData, setTimeseriesData] = useState<WeatherData['properties']['timeseries'][number] | null>(null);
+export function useTimeseriesData(
+  destinationPoint: DestinationPoint,
+  index: number
+) {
+  const {
+    data,
+    isLoading,
+    isError,
+    error
+  } = useWeatherDataQuery(destinationPoint);
 
-  useEffect(() => {
-    if (!isLoading && !isError && data?.properties.timeseries) {
-      // Access the data at the specified index
-      setTimeseriesData(data.properties.timeseries[index] || null);
-    }
-  }, [data, isLoading, isError, index]);
+  if (isLoading || isError) {
+    return {
+      isLoading, isError, error
+    };
+  }
 
-  return { timeseriesData, isLoading, isError };
-};
+  const timeseriesData = data.properties.timeseries[index];
+
+
+  return {
+    timeseriesData,
+    isLoading,
+    isError,
+    error
+  };
+}
